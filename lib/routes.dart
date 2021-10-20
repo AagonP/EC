ee68f0e1ae5a31666a594cd861ab0a33444575bc
gitHub882000 <gitHub882000>
2021-10-20 08:56:12 +0700

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/widgets.dart';
 import 'package:shop_app/screens/cart/cart_screen.dart';
+import 'package:shop_app/screens/category/category_screen.dart';
 import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
 import 'package:shop_app/screens/details/details_screen.dart';
 import 'package:shop_app/screens/store/store_screen.dart';
+import 'package:shop_app/screens/favourite/fav_screen.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shop_app/screens/home/home_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
 import 'package:shop_app/screens/otp/otp_screen.dart';
 import 'package:shop_app/screens/profile/profile_screen.dart';
+import 'package:shop_app/screens/search/search_screen.dart';
 import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
 import 'package:shop_app/screens/splash/splash_screen.dart';
 
@@ -24,8 +27,13 @@ final Map<String, WidgetBuilder> routes = {
   CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
   OtpScreen.routeName: (context) => OtpScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
-  StoreScreen.routeName: (context) => StoreScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
   CartScreen.routeName: (context) => CartScreen(),
   ProfileScreen.routeName: (context) => ProfileScreen(),
+
+  // Add route here
+  CategoryScreen.routeName: (context) => CategoryScreen(),
+  FavouriteScreen.routeName: (context) => FavouriteScreen(),
+  SearchScreen.routeName: (context) => SearchScreen(),
+  StoreScreen.routeName: (context) => StoreScreen(),
 };
