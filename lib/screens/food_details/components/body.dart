f5fdb1b520ff83d370348f62060d4c20273653d2
Phạm Minh Hiếu <54505544+AagonP@users.noreply.github.com>
2021-10-21 16:13:59 +0700

    Add item to cart function:x

--- lib/screens/food_details/components/body.dart
+++ lib/screens/food_details/components/body.dart
@@ -7,6 +7,8 @@ import 'quantity_config.dart';
 import 'food_description.dart';
 import 'top_rounded_container.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Body extends StatelessWidget {
   final Food food;
 
@@ -47,7 +49,9 @@ class Body extends StatelessWidget {
                         ),
                         child: DefaultButton(
                           text: "Add To Cart",
-                          press: () {},
+                          press: () {
+                            addItemToCart();
+                          },
                         ),
                       ),
                     ),
@@ -60,4 +64,44 @@ class Body extends StatelessWidget {
       ],
     );
   }
+}
+
+void addItemToCart() {
+  final uid = 'mock_user_id';
+  final food_id = 'food_id';
+  // Food selected will be post to orders collection in Firestore
+  // If the food item is not in the cart
+  CollectionReference orders = FirebaseFirestore.instance
+      .collection('orders')
+      .doc(uid)
+      .collection('foods');
+  orders.doc(food_id).get().then(
+    (DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        // Food item already in cart
+        final String new_amount =
+            (int.parse(documentSnapshot['quanity']) + 1).toString();
+        orders.doc(food_id).update(
+          {
+            'quanity': new_amount,
+          },
+        ).catchError((error) => print("Failed to update item to cart: $error"));
+      } else {
+        // Set new documentID for new item
+        orders.doc(food_id).set(
+          {
+            'quanity': '1',
+          },
+        ).catchError((error) => print("Failed to add item to cart: $error"));
+      }
+    },
+  );
+  // Update total number of items
+  orders.doc('no_item').get().then((DocumentSnapshot documentSnapshot) {
+    final String new_total_items =
+        (int.parse(documentSnapshot['no_item']) + 1).toString();
+    orders.doc('no_item').update({
+      'no_item': new_total_items,
+    });
+  });
 }
